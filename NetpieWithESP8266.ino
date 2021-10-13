6dc996bf902c8794d43425f463153b242e758bf1
Prachya Thongsan <70775537+LittleLunar@users.noreply.github.com>
2021-10-13 18:33:25 +0700

    update source code

--- NetpieWithESP8266.ino
+++ NetpieWithESP8266.ino
@@ -1,10 +1,16 @@
 
-#define MY_WIFI_NAME "YOUR_WIFI_NAME"
-#define MY_WIFI_PASS "YOUR_WIFI_PASS"
+#define MY_WIFI_NAME "YourWiFiName"
+#define MY_WIFI_PASS "YourWiFiPass"
 
+// Use the topic as you wish
 #define SUBSCRIBE_TOPIC ""
 #define PUBLISH_TOPIC ""
 
+#define mqtt_server "broker.netpie.io"
+#define mqtt_Client ""
+#define mqtt_username ""
+#define mqtt_password ""
+#define mqtt_port 1883
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
@@ -16,67 +22,79 @@
 
 const char* ssid = MY_WIFI_NAME;
 const char* pass = MY_WIFI_PASS;
-const char* mqtt_server = "broker.netpie.io";
-const int mqtt_port = 1883;
-const char* mqtt_Client = "";
-const char* mqtt_username = "";
-const char* mqtt_password = "";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-DHT dht(DHTPIN, DHTTYPE);
-char msg[50];
 
-void reconnect() {
-  while(!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if(client.connect(mqtt_Client, mqtt_username, mqtt_password) {
+DHT dht(DHTPIN, DHTTYPE);
+
+void reconnect() 
+{
+  
+  while( !client.connected() ) 
+  {
+    Serial.print("Reconnecting...");
+    
+    if( client.connect(mqtt_Client, mqtt_username, mqtt_password ) )
+    {
       Serial.println("Connected");
       client.subscribe(SUBSCRIBE_TOPIC);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println("try again in 5 seconds");
+    } 
+    else
+    {
+      Serial.println("Connection failed... try again in 5 seconds");
       delay(5000);
     }
+       
   }
+       
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  String message;
-  for(int i = 0; i < length; i++) {
-    message = message + (char)payload[i];
-  }
-  Serial.println(message);
-  if(String(topic) == SUBSCRIBE_TOPIC) {
-    if(message == "on") {
-      digitalWrite(LED1, 0);
-      client.publish(PUBLISH_TOPIC, "");
-      Serial.println("LED on");
-    }
-    else if(message == "off") {
-      digitalWrite(LED1, 1);
-      client.publish(PUBLISH_TOPIC, "");
-      Serial.println("LED off");
-    }
-  }
-}
-void setup(){
   
+  String msg;
+  
+  for(int i = 0; i < length; i++)
+    msg += (char)payload[i];
+  
+  Serial.println(msg);
+  
+  if(msg == "on") 
+  {
+    digitalWrite(LED1, 0);
+    client.publish(PUBLISH_TOPIC, "");
+    Serial.println("LED on");
+  }
+  else if(msg == "off") 
+  {
+    digitalWrite(LED1, 1);
+    client.publish(PUBLISH_TOPIC, "");
+    Serial.println("LED off");
+  }
+    
+  
+}
+void setup()
+{
   Serial.begin(115200);
 
   pinMode(LED1, OUTPUT);
   
   Serial.print("\nConnecting to ");
   Serial.println(ssid);
+  
   WiFi.begin(ssid, password);
+  
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  
   Serial.println("\nWiFi connected");
   Serial.print("IP address : ");
   Serial.println(WiFi.localIP());
@@ -87,17 +105,21 @@ void setup(){
   dht.begin();
 }
 
-void loop() {
+void loop() 
+{
   int humidity = dht.readHumidity();
   int temperature = dht.readTemperature();
-  if (!client.connected()) {
+  
+  if ( !client.connected() ) 
     reconnect();
-  }
-  client.stop();
-  String data = "";
-  Serial.println(data);
-  data.toCharArray(msg, (data.length() + 1);
+  
+  String data = ""; // data to be sent that depend on schema properties in netpie device
+  char msg[data.length() + 1];
+  data.toCharArray(msg, data.length());
+  
   client.publish(PUBLISH_TOPIC, msg);
+  
+  Serial.println("Send : [" + data + "]");
   delay(2000);
   
 }
